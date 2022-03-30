692ff7193f7b8de5cb2e33cd01a7122d6a258289
daekyu.park <mamma1234@gmail.com>
2022-03-30 09:41:14 +0900

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
+
+<div>{{ phpinfo() }}</div>
+<div>{{ xdebug_info() }}</div>
+
 @section('content')
     <div class="flex-1">
         <the-chat :current-user="{{ auth()->id() }}"></the-chat>
